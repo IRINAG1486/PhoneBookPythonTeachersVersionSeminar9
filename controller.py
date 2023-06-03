54ca13be817185fa78098bd41195fa6edcc92c96
IRINAG <ira848@mail.ru>
2023-06-03 12:56:29 +0600

    .\controller.py

--- controller.py
+++ controller.py
@@ -4,44 +4,44 @@ import text
 #делаем функцию старт
 
 def start():
+
+    my_pb = model.PhoneBook()
     while True:
         choice = view.main_menu()
         match choice:
             case 1:
-                model.open_pb()
+                my_pb.open()
                 view.print_message(text.load_successful)
             case 2:
-                model.save_pb()
-               
+                my_pb.save()
                 view.print_message(text.save_successful)
             case 3:
-                
-                pb = model.get_pb()
+                pb = my_pb.load()
                 view.print_contact(pb, text.load_error)
             case 4:
                 contact = view.input_contact(text.new_contact, text.cancel_input)
-                name = model.add_contact(contact)
+                name = my_pb.add(contact)
                 view.print_message(text.new_contact_successful(name))
             case 5:
-                pb = model.get_pb()
+                pb = my_pb.load()
                 info = view.input_find_contact()
-                your_contact = model.find_contact(pb, info, text.find_contact_error)
+                your_contact = my_pb.find(pb, info, text.find_contact_error)
                 view.print_contact(your_contact, text.find_contact_error)
 
             case 6:
-                pb = model.get_pb()
+                pb = my_pb.load()
                 info = view.input_find_contact()
                 view.print_contact(pb, text.load_error)
                 name = view.input_contact(text.new_contact, text.cancel_input)
-                change_pb = model.change_contact(info, name, text.find_contact_error)
+                change_pb = my_pb.change(info, name, text.find_contact_error)
                 view.print_message(text.change_pb)
                 
-                pass
             case 7:
-                pb = model.get_pb()
+                pb = my_pb.load()
                 index = view.input_index(text.index_del_contact, pb, text.load_error)
-                name = model.del_contact(index)
+                name = my_pb.dellcont(index)
                 view.print_message(text.del_contact(name))
+                
             case 8:
                 view.print_message(text.close_pb)
                 break
