28e4ac5cee8d72895c7c13453b07d4b659b3825d
IRINAG <ira848@mail.ru>
2023-06-03 13:00:04 +0600

    .\model.py

--- model.py
+++ model.py
@@ -1,73 +1,67 @@
-phone_book: list[dict[str, str]] = []
-path = 'phone.txt'
+class PhoneBook:
 
+    phone_book: list[dict[str, str]] = []
+    path = 'phone.txt'
 
-#2 открыть файл
-def open_pb():
-    global phone_book, path
-    with open(path, 'r', encoding='UTF-8') as file:
-        data = file.readlines()
-        #print(data)
-    for contact in data:
-        contact = contact.strip().split(':')
-        phone_book.append({'name':contact[0], 'phone':contact[1], 'comment':contact[2]})
-            #phone_book.append(contact)
-    #print(phone_book)
+    def __init__(self, path: str = 'phone.txt'):
+        self.phone_book: list[dict[str, str]] = []
+        self.path = path
 
-
-#3 показать контакт
-def get_pb() -> list[dict[str, str]]:
-    global phone_book
-    return phone_book
-
-#добавить контакт
-def add_contact(contact: dict[str, str]):
-    global phone_book
-    phone_book.append(contact)
-    return contact.get('name')
-
-#записать контакт
-def save_pb():
-    global phone_book, path
-    data = []
-    for contact in phone_book:
-        contact = ':'.join([value for value in contact.values()])
-        data.append(contact)
-    with open(path, 'w', encoding='UTF-8') as file:
-        file.write('\n'.join(data))
-
-#удалить контакт
-def del_contact(index: int):
-    return phone_book.pop(index - 1).get('name')
-
-#найти контакт
-
-def find_contact(pb: list[dict[str, str]], message: str, error: str):
-    global phone_book, path
-    data: list[dict[str, str]] = []
-    for contact in phone_book:
-        for value in contact.values():
-            if message.lower() in value.lower():
-                data.append(contact)
-                #print(data)
-                return data
-            else:
-                error
    
+    def open(self):
+        with open(self.path, 'r', encoding='UTF-8') as file:
+            data = file.readlines()
+        for contact in data:
+            contact = contact.strip().split(':')
+            self.phone_book.append({'name':contact[0], 'phone':contact[1], 'comment':contact[2]})
+                
+
+    
+    def load(self):
+        return self.phone_book
+
+   
+    def add(self, contact: dict[str, str]):
+        self.phone_book.append(contact)
+        return contact.get('name')
+
+  
+    def save(self):
+        data = []
+        for contact in self.phone_book:
+            contact = ':'.join([value for value in contact.values()])
+            data.append(contact)
+        with open(self.path, 'w', encoding='UTF-8') as file:
+            file.write('\n'.join(data))
+
+    
+    def dellcont(self, index: int):
+        return self.phone_book.pop(index - 1).get('name')
 
 
 
-#изменить контакт
-def change_contact(info_contact, dict_contact: dict[str, str], error):
-    global phone_book, path
-    #print(dict_contact)
-    for contact in phone_book:
-        for value in contact.values():
-            if info_contact.lower() in value.lower():
-                contact['name'] = dict_contact.get('name')
-                contact['phone'] = dict_contact.get('phone')
-                contact['comment'] = dict_contact.get('comment')
-            else:
-                error
-    return phone_book
+    def find(self, pb: list[dict[str, str]], message: str, error: str):
+        data: list[dict[str, str]] = []
+        for contact in self.phone_book:
+            for value in contact.values():
+                if message.lower() in value.lower():
+                    data.append(contact)
+                    return data
+                else:
+                    error
+    
+
+
+
+  
+    def change(self, info_contact, dict_contact: dict[str, str], error):
+        for contact in self.phone_book:
+            for value in contact.values():
+                if info_contact.lower() in value.lower():
+                    contact['name'] = dict_contact.get('name')
+                    contact['phone'] = dict_contact.get('phone')
+                    contact['comment'] = dict_contact.get('comment')
+                else:
+                    error
+        return self.phone_book
                
